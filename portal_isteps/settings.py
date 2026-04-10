7d04359ad87653105db5bde72667cd37e6413781
Jordy13032001 <jordy.briceno@est.ucacue.edu.ec>
2026-04-10 09:53:07 -0500

    Agregar representante estudiantes

--- portal_isteps/settings.py
+++ portal_isteps/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-default-key-12345")
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
 
