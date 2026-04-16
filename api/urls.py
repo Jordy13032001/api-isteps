5633290d9bb735a8dbdefeb82c1b414ba97273cf
Jordy13032001 <jordy.briceno@est.ucacue.edu.ec>
2026-04-16 09:25:38 -0500

    Agrega endpoint estudiantes curso

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
-from .views import estudiantes_curso
+from api.views.content_views import estudiantes_curso
 
 from api.views.content_views import cursos_moodle
 
